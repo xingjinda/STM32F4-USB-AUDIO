b886ef44befd8e06d36ac4550de211546da37069
xjd <88171164+xingjinda@users.noreply.github.com>
2024-07-08 19:25:29 +0800

    Update IIC.h

--- hardware/IIC.h
+++ hardware/IIC.h
@@ -2,7 +2,7 @@
 #define __IIC_H
 #include "stm32f4xx.h" 
 
-#define IIC_CLOCK()     RCC->AHB1ENR|=1<<2;//开启GPIOC时钟
+#define IIC_CLOCK()		RCC->AHB1ENR|=1<<2;//开启GPIOC时钟
 
 #define	SCL_GPIO	GPIOC
 #define	SCL_PIN		4
@@ -11,53 +11,19 @@
 #define SDA_PIN		5
 
 
-#define SDA_IN()  {SDA_GPIO->MODER&=~(3<<(SDA_PIN*2));SDA_GPIO->MODER|=0<<(SDA_PIN*2);}
-#define SDA_OUT() {SDA_GPIO->MODER&=~(3<<(SDA_PIN*2));SDA_GPIO->MODER|=1<<(SDA_PIN*2);}
+#define SDA_IN()	{SDA_GPIO->MODER&=~(3<<(SDA_PIN*2));SDA_GPIO->MODER|=0<<(SDA_PIN*2);}
+#define SDA_OUT()	{SDA_GPIO->MODER&=~(3<<(SDA_PIN*2));SDA_GPIO->MODER|=1<<(SDA_PIN*2);}
 
 	 
-#define READ_SDA		SDA_GPIO->IDR&1<<SDA_PIN
-#define IIC_SCL(x)		if(x)SCL_GPIO->ODR|=(1<<SCL_PIN); else SCL_GPIO->ODR&=~(1<<SCL_PIN);
-#define IIC_SDA(x)		if(x)SDA_GPIO->ODR|=(1<<SDA_PIN); else SDA_GPIO->ODR&=~(1<<SDA_PIN);
+#define READ_SDA	SDA_GPIO->IDR&1<<SDA_PIN
 
 void IIC_Init(void);		 
 void IIC_Start(void);	
 void IIC_Stop(void);
-void IIC_Send(unsigned char txd);
-unsigned char IIC_Read(unsigned char ack);
-unsigned char IIC_Wait_Ack(void); 
+void IIC_Send(uint8_t txd);
+uint8_t IIC_Read(uint8_t ack);
+uint8_t IIC_Wait_Ack(void); 
 void IIC_Ack(void);	
 void IIC_NAck(void);
 
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
